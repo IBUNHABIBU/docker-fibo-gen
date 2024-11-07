291692424d4a776ff6aadb92d3b00300d82ebe3f
IBUNHABIBU <ibunhabib@gmail.com>
2024-11-07 19:03:59 +0300

    Add category

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema[8.0].define(version: 2024_09_20_191524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
@@ -58,6 +59,11 @@ ActiveRecord::Schema[8.0].define(version: 2024_09_20_191524) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
+=======
+ActiveRecord::Schema[7.2].define(version: 2024_11_07_151221) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+>>>>>>> 61ba191 (Add category)
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -65,6 +71,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_09_20_191524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+<<<<<<< HEAD
 
   create_table "likes", force: :cascade do |t|
     t.bigint "article_id", null: false
@@ -88,4 +95,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_09_20_191524) do
   add_foreign_key "articles", "users"
   add_foreign_key "likes", "articles"
   add_foreign_key "likes", "users"
+=======
+>>>>>>> 61ba191 (Add category)
 end
