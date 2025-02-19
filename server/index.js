db36fa3ed407159dcd5910022365ebe076a7770c
IBUNHABIBU <ibunhabib@gmail.com>
2025-02-19 04:13:47 +0300

    Add routes

--- server/index.js
+++ server/index.js
@@ -60,5 +60,6 @@ app.post('/values', async (req, res) => {
     }
 
     redisClient.hset('values', index, 'Nothing yet');
-    redisPublisher.publish()
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
 })
