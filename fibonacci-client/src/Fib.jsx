6fe1216fb7aaa83ba0e0aa6214c0c12bbd1767af
IBUNHABIBU <ibunhabib@gmail.com>
2025-02-21 21:43:51 +0300

    Add react ui

--- fibonacci-client/src/Fib.jsx
+++ fibonacci-client/src/Fib.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export default class Fib extends Component {
-  static propTypes = {second: third}
+ 
 
   render() {
     return (
